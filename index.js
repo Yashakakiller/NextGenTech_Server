4aa56a82f7228bafc8efba551fe8547a20747c6a
Yash Gupta <yashakakiller@gmail.com>
2024-08-20 12:35:44 +0530

    removing limit

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const bodyparser = require('body-parser');
-const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const { Connection } = require('./database/db');
 const { userRouter } = require('./routes/UserRoutes');
@@ -18,17 +17,12 @@ const PORT = process.env.PORT || 5000;
 Connection();
 const app = express();
 
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per windowMs
-  message: 'Too many requests from this IP, please try again later.'
-});
+
 
 app.use(bodyparser.json({ extended: true, limit: '100mb' }));
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(helmet());
-app.use(limiter);
 
 app.use('/accounts/user', userRouter);
 app.use('/categories', categoryRouter);
